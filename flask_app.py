2965c8bac61e77a21d4cfd0a61224849ddc5efaf
tommydebisi <tommydebisi@gmail.com>
2022-10-28 10:50:14 +0000

    :white_check_mark: app not restricted by CORS

---
+++ flask_app.py
@@ -0,0 +1,21 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/', strict_slashes=False)
+def hello_world():
+    if request.method == "GET":
+        new_dict = {
+            'slackUsername': 'TommyOnCode',
+            'backend': True,
+            'age': 21,
+            'bio': "I'm a Software Engineer, with intermediary knowledge of python"
+            }
+    return jsonify(new_dict)
+
